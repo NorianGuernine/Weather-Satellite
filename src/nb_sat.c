6fcbc8c01d01990345a7632d410cea42d1bb6deb
Norian <n.guernine@outlook.fr>
2022-01-11 21:17:33 +0100

    Ajout du chemin vers les librairies python dans nb_sat.c

--- src/nb_sat.c
+++ src/nb_sat.c
@@ -59,9 +59,7 @@ int main(int argc, char *argv[]){
  *  * nouvelle tâche. Cependant à terme la partie Python sera réécrite en 
  *  C++ donc ce sera intéressant d'avoir une gestion                       
  *   * des tâches à ce moment */                                           
-void *task(void *arg){                                                 
-	char filename[] = "METEO.py";                                   
-	FILE *fp;
+void *task(void *arg){                                                                                   
    	info_radio *infos = (info_radio*) arg;
 	uint32_t freq= infos->freq;
 	char *date = infos->date;
@@ -72,10 +70,13 @@ void *task(void *arg){
 	struct tm tm = *localtime(&t);
 	bool LOCK = true;
 	int stop_python;
+
 	Py_Initialize();
-	PyObject *Module = PyUnicode_FromString("test");
-	PyObject *librairie = PyImport_Import(Module);
-                                                                              
+        PyObject *pName, *pModule, *pFunc, *pArgs, *pValue;
+        PyObject* sys = PyImport_ImportModule("sys");
+	PyObject* path = PyObject_GetAttrString(sys, "path");
+	PyList_Insert(path, 0, PyUnicode_FromString("."));
+	
 	while(true){    		
 		sleep(1);
 		time(&t);
@@ -86,22 +87,18 @@ void *task(void *arg){
 			if(strcmp(date,sys_date) <= 0){
 				pthread_mutex_lock(&RTL2832U);
 				fprintf(stderr,"Lancer le soft et prendre mutex \n");
-				PyObject* myFunction = PyObject_GetAttrString(librairie,(char*)"main");
-				//On passe les arguments à la fonction main python
-				//PyObject* args = PyTuple_Pack(2,PyLong_FromLong((long)freq),PyLong_FromString(name,NULL,0));
-				//PyObject* args = Py_BuildValue("(is)", freq, name);
-				PyObject* args = PyTuple_New(freq);
-				PyObject* myResult = PyObject_CallObject(myFunction, args);
-				//PyObject* args = Py_BuildValue("(z)",(char*)"something");
-				fprintf(stderr,"passer\n");		
-				//PrgsyRun_SimpleFile(fp, filename);
+        			pName = PyUnicode_FromString((char*)"main");
+	        		pModule = PyImport_Import(pName);
+		        	pFunc = PyObject_GetAttrString(pModule, (char*)"main");
+			        pArgs = Py_BuildValue("(s)",name);
+				pValue = PyObject_CallObject(pFunc, pArgs);
 				Py_Finalize();
+				fprintf(stderr,"passer\n");		
 				LOCK = false;
 			}
 		}
 		if(strcmp(end_date,sys_date) <= 0){
 			fprintf(stderr,"Stopper le soft et rendre mutex \n");
-			stop_python = raise(SIGINT);
 			pthread_mutex_unlock(&RTL2832U); 
 		}
 	}
